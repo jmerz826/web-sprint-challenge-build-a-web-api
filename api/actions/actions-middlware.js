748986a92dcad7df6a95dcd4286c02a6127ab4b4
John Merz <jmerz826@gmail.com>
2022-01-14 13:48:44 -0500

    validate action id middleware and get action by id endpoint built

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,16 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActionId(req, res, next) {
+    const {id} = req.params
+    const success = await Action.get(id)
+
+    if (!success) res.status(404).json({ message: `Action with id ${id}` })
+    
+    else {
+        req.actions = success
+        next()
+    } 
+}
+
+module.exports = {validateActionId}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,12 +4,22 @@ const router = express.Router()
 
 const Action = require('./actions-model')
 
+const {validateActionId} = require('./actions-middlware')
+
 router.get('/', async (req, res, next) => {
     const actions = await Action.get()
 
     if (!actions) next()
         
     else res.status(200).json(actions)
+})
+
+router.get('/:id', validateActionId, (req, res, next) => {
+    try {
+        res.status(200).json(req.actions)
+    } catch {
+        next()
+    }
 })
 
 module.exports = router

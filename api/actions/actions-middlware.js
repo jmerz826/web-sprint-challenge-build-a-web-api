cabb4e7ac56309609c1bfc82483163037c0e996e
John Merz <jmerz826@gmail.com>
2022-01-14 14:39:35 -0500

    actions middleware and router cleaned and commented

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,30 +1,37 @@
-// add middlewares here related to actions
 const Action = require('./actions-model')
 const Project = require('../projects/projects-model')
 
 async function validateActionId(req, res, next) {
-    const {id} = req.params
+    const { id } = req.params
+    
+    // fetch action with passed in ID
     const success = await Action.get(id)
 
+    // returns 404 status if action with specified ID does not exist
     if (!success) res.status(404).json({ message: `Action with id ${id}` })
     
     else {
-        req.actions = success
-        next()
+        req.action = success // Sets req.action to the action with ID passed in
+        next() // continue
     } 
 }
 
 async function validateNewAction(req, res, next) {
     const { project_id, description, notes, completed } = req.body
-    const project = await Project.get(project_id)
 
+    // check if the project with specified project_id actually exists
+    const project = await Project.get(project_id) 
+
+    // reuturn 404 if project with project_id does not exist
     if (!project) res.status(404).json({ message: `Cannot find project with id of ${project_id}` })
     else {
+        // new action must contain a description, notes, and either true or false for 'completed'
+        // if any of these criteria aren't met, return 400 status
         if (!description || !description.trim() || !notes || !notes.trim() || completed === undefined) {
             res.status(400).json({message: 'description, notes, and completion status are required'})
         } else {
             req.newAction = {project_id, description, notes, completed}
-            next()
+            next() // continue
         }
     }
 }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,50 +1,48 @@
 // Write your "actions" router here!
 const express = require('express')
 const router = express.Router()
-
 const Action = require('./actions-model')
-
 const {validateActionId, validateNewAction} = require('./actions-middlware')
 
 router.get('/', async (req, res, next) => {
     const actions = await Action.get()
 
-    if (!actions) next()
-        
-    else res.status(200).json(actions)
+    if (!actions) next() // if no actions returned, continue to default fail catch in server file
+    else res.status(200).json(actions) // respond with array of actions
 })
 
 router.get('/:id', validateActionId, (req, res, next) => {
     try {
-        res.status(200).json(req.actions)
+        res.status(200).json(req.action) // responds with action with passed in ID
     } catch {
-        next()
+        next() // continue to default fail catch in server file
     }
 })
 
 router.post('/', validateNewAction, (req, res, next) => {
     Action.insert(req.newAction)
         .then(added => {
-            res.status(200).json(added)
+            res.status(200).json(added) // respond with newly created action
         })
-        .catch(err => next(err))
+        .catch(() => next()) // continue to default fail catch in server file
 })
 
 router.put('/:id', validateActionId, validateNewAction, (req, res, next) => {
     Action.update(req.params.id, req.newAction)
         .then(updated => {
-            res.status(200).json(updated)
+            res.status(200).json(updated) // responds with updated action
         })
-        .catch(err => next(err))
+        .catch(() => next()) // continue to default fail catch in server file
 })
 
 router.delete('/:id', validateActionId, (req, res, next) => {
     const {id} = req.params
     Action.remove(id)
         .then(() => {
+            // respond with success message
             res.status(200).json({message: `Action with id ${id} deleted successfully`})
         })
-        .catch(err => next(err))
+        .catch(() => next()) // continue to default fail catch in server file
 })
 
 module.exports = router
